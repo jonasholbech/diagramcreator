bea141a19d147f5cc1c2da498f5a3e943df5c155
Jonas Holbech <jofh@kea.dk>
2020-01-18 23:32:45 +0100

    wrapped up nicely

--- js/parser.js
+++ js/parser.js
@@ -1,79 +1,94 @@
 //https://github.com/bramp/js-sequence-diagrams/blob/master/README.md
-let style = "simple";
-
-function fetchData(path) {
-  fetch("samples/" + path)
-    .then(res => res.text())
-    .then(parseData);
-}
-function setStyle(newStyle) {
-  style = newStyle;
-}
-function parseData(data) {
-  //let diagramData = [];
-  let functions = [];
-  let findAllFunctions = /function\s+([a-zA-Z]*)\((.*)\)\s*{/g;
-  const matches = data.matchAll(findAllFunctions);
-
-  //find function name, arguments & function body
-  for (const match of matches) {
-    //console.log(match);
-    const restOfFile = data.substring(match.index);
-    let openBrackets = 0;
-    let functionAsString = "";
-    const restOfFileAsArray = restOfFile.split("\n");
-    for (let i = 0; i < restOfFileAsArray.length; i++) {
-      let line = restOfFileAsArray[i];
-      if (line.includes("{")) {
-        openBrackets++;
-      }
-      if (line.includes("}")) {
-        openBrackets--;
-      }
-
-      if (openBrackets === 0) {
-        functionAsString = restOfFileAsArray.slice(0, i).join("\n");
-        break;
-      }
-    }
-    const func = {
-      name: match[1],
-      arguments: match[2].split(","),
-      functionDefinition: functionAsString,
-      calls: []
-    };
-    functions.push(func);
+class DiagramCreator {
+  constructor(selector, path = "", theme = "simple") {
+    this.selector = selector;
+    this.parseData = this.parseData.bind(this);
+    this.setTheme(theme);
+    this.setPath(path);
   }
-  show(functions);
-}
-function show(functions) {
-  const functionNames = functions.map(func => func.name);
+  fetchData() {
+    document.querySelector("#" + this.selector).innerHTML = "Loading";
+    fetch("samples/" + this.path)
+      .then(res => res.text())
+      .then(this.parseData);
+  }
+  setTheme(newTheme) {
+    this.theme = newTheme;
+    if (this.path) {
+      this.fetchData();
+    }
+  }
+  setPath(newPath) {
+    this.path = newPath;
+    this.fetchData();
+  }
+  parseData(data) {
+    //let diagramData = [];
+    let functions = [];
+    let findAllFunctions = /function\s+([a-zA-Z]*)\((.*)\)\s*{/g;
+    const matches = data.matchAll(findAllFunctions);
 
-  //With all functions found (hopefully), find function calls within each function
-  functions.forEach(func => {
-    functionNames.forEach(fn => {
-      if (fn != func.name) {
-        //recursion is not an option here!
-        if (func.functionDefinition.includes(fn)) {
-          func.calls.push(fn);
+    //find function name, arguments & function body
+    for (const match of matches) {
+      //console.log(match);
+      const restOfFile = data.substring(match.index);
+      let openBrackets = 0;
+      let functionAsString = "";
+      const restOfFileAsArray = restOfFile.split("\n");
+      for (let i = 0; i < restOfFileAsArray.length; i++) {
+        let line = restOfFileAsArray[i];
+        if (line.includes("{")) {
+          openBrackets++;
+        }
+        if (line.includes("}")) {
+          openBrackets--;
+        }
+
+        if (openBrackets === 0) {
+          functionAsString = restOfFileAsArray.slice(0, i).join("\n");
+          break;
         }
       }
-    });
-  });
-  //console.log("functions:", functions);
-
-  let diagramData = [];
-  functions.forEach(func => {
-    if (func.calls.length === 0) {
-      diagramData.push(`participant ${func.name}`);
-    } else {
-      func.calls.forEach(call => {
-        diagramData.push(`${func.name}->${call}: ${func.arguments.join(", ")}`);
-      });
+      const func = {
+        name: match[1],
+        arguments: match[2].split(","),
+        functionDefinition: functionAsString,
+        calls: []
+      };
+      functions.push(func);
     }
-  });
-  console.log(diagramData);
-  var diagram = Diagram.parse(diagramData.join("\n"));
-  diagram.drawSVG("diagram", { theme: style }); //hand, simple
+    this.show(functions);
+  }
+  show(functions) {
+    const functionNames = functions.map(func => func.name);
+
+    //With all functions found (hopefully), find function calls within each function
+    functions.forEach(func => {
+      functionNames.forEach(fn => {
+        if (fn != func.name) {
+          //recursion is not an option here!
+          if (func.functionDefinition.includes(fn)) {
+            func.calls.push(fn);
+          }
+        }
+      });
+    });
+    //console.log("functions:", functions);
+
+    let diagramData = [];
+    functions.forEach(func => {
+      if (func.calls.length === 0) {
+        diagramData.push(`participant ${func.name}`);
+      } else {
+        func.calls.forEach(call => {
+          diagramData.push(
+            `${func.name}->${call}: ${func.arguments.join(", ")}`
+          );
+        });
+      }
+    });
+    document.querySelector("#" + this.selector).innerHTML = "";
+    var diagram = Diagram.parse(diagramData.join("\n"));
+    diagram.drawSVG(this.selector, { theme: this.theme }); //hand, simple
+  }
 }
-document.querySelector("h1").textContent = "Done";
